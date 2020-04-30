cdb008db13baf9d746aeda1e2bb5a8f6bebdee08
kapilkus <64282061+kapilkus@users.noreply.github.com>
2020-04-30 11:24:51 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+
+  <!-- Font Awesome CSS -->
+  <link rel="stylesheet" href="css/all.min.css">
+
+  <!-- Google Font -->
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville&display=swap" rel="stylesheet">
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="css/custom.css">
+
+  <title>Qone_Technology</title>
+</head>
+
+<body>
+  <!-- Start Navigation -->
+  <nav class="navbar navbar-expand-sm navbar-dark bg-primary pl-5 fixed-top">
+    <a href="index.php" class="navbar-brand">Qone Technology</a>
+    <!-- <span class="navbar-text">Customer's Happiness is our Aim</span> -->
+    <span class="navbar-text">Complete IT Solution</span>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myMenu">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="myMenu">
+      <ul class="navbar-nav pl-5 custom-nav text-uppercase ml-auto">
+        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item" for="cars"><a href="#Services" class="nav-link">Services</a></li>  
+             
+        <li class="nav-item"><a href="#registration" class="nav-link">Registration</a></li>
+        <li class="nav-item"><a href="Requester/RequesterLogin.php" class="nav-link">Login</a></li>
+        <li class="nav-item"><a href="#Contact" class="nav-link">Contact</a></li>
+        <a href="Requester/RequesterLogin.php" class="btn btn-success mr-4">Login</a>
+      <a href="#registration" class="btn btn-success mr-4">Sign Up</a>        
+      </ul>      
+    </div>
+  </nav> <!-- End Navigation -->
+
+  <!-- Start Header Jumbotron-->
+  <header class="jumbotron back-image" style="background-image: url(images/banner1.jpg);">
+    <div class="myclass mainHeading">
+      <h1 class="text-uppercase text-danger font-weight-bold">Welcome to <br> QONE TECHNOLOGY</h1>
+      <p class="font-italic">Customer's Happiness is our Aim</p>
+      <p class="font-italic">Now you can book your service online <br>by doing Registration</p>
+      <a href="#registration" class="btn btn-success mr-4">Click Here</a>    
+      </div>
+  </header> <!-- End Header Jumbotron -->
+
+
+<section id="about">
+  
+    <!--Introduction Section-->
+    <!-- <div class="jumbotron back-image" style="background-image: url(images/banner.jpg);"> -->
+    <div class="container" >
+      <h3 class="text-center">About Us</h3>
+      <p>
+      Is your computer down and not working? Are you facing a problem with your computer software? 
+      Get our excellent Onsite Technical Support services. At Proponent Technologies, we provide you 
+      cost-effective, fast and reliable method of fixing your personal computer and Laptop. We do not 
+      charge any fee until your problem gets resolved by our Onsite Technical Support technician. 
+      We always take your system privacy and security on priority and always provides you a fast and 
+      complete PC solutions related to hardware and software support. Our on-site technical support 
+      staff is a dedicated team of experts who always work for the best resolutions of your system problem. 
+      No matter what problem you are facing with your system. Our expert always provides you best Onsite 
+      Technical Support on demand after an onsite visit. It makes the whole resolution process fast and 
+      cost-effective. The services we provide through Onsite Technical Support are as follows: 
+      PC operating system support System software support Application software support Peripheral connectivity 
+      and other devices support Software installation services.
+      </p>
+      <h4 class="text-center">Best Business Class Technical Support Since 2014</h4>
+      <center><p>Our goal is to satisfy every customer. 
+      We believe in creating lasting relationships and when you succeed, we succeed</p>
+
+      <h3 class="text-center">Computer & Laptop Service Center</h3>
+      <p>HP Dell Lenovo Sepeclist</p>
+
+    <!-- </div> -->
+  </div>
+  </section>
+  <br>
+  <br>
+  <!--Introduction Section End-->
+  <!-- Start Services -->
+  <div class="container text-center border-bottom" id="Services">
+    <h2>Our Services</h2><br>
+    <div class="row mt-4">
+      <div class="col-sm-4 mb-4">
+        <a href="#"><img src="images/com-computer.gif" alt=""></a>
+        <h4 class="mt-4">Computer Laptop Repairing</h4>
+      </div>
+      <div class="col-sm-4 mb-4">
+      
+        <a href="#"><i class="fas fa-plug fa-8x text-primary"></i></a>
+        <h4 class="mt-4">Electronic Appliences Repairing</h4>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-tv fa-8x text-primary"></i></a>
+        <h4 class="mt-4">Computer Laptop Maintenance & Servicing</h4>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-tv fa-8x text-primary"></i></a>
+        <h4 class="mt-4">Windows & Software Maintenance</h4>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-sync-alt fa-spin fa-8x text-primary"></i></a>
+        <h4 class="mt-4">Data Recovery</h4>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <a href="#"><img src="images/com-window.png" alt="" style="width:200px;height:120px;"></a>
+        <h4 class="mt-4">Web & Software Development</h4>
+      </div>
+      <div class="col-sm-4 mb-4" class="img2">
+        <a href="#"><img src="images/com-androide.gif" alt="" style="width:200px;height:120px;"></a>
+        <!-- <a href="#"><i class="fas fa-laptop fa-8x text-primary"></i></a> -->
+        <h4 class="mt-4">Androide Development</h4>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-sliders-h fa-8x text-primary"></i></a>
+        <h4 class="mt-4">Preventive Maintenance</h4>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-cogs fa-spin fa-8x text-info"></i></a>
+        <h4 class="mt-4">AMC Service</h4>
+      </div>
+    </div>
+  </div> <!-- End Services -->
+
+  <!-- Start Registration  -->
+  <?php include('userRegistration.php') ?>
+  <!-- End Registration  -->
+
+  <!-- Start Happy Customer  -->
+  <div class="jumbotron bg-primary" id="Customer">
+    <!-- Start Happy Customer Jumbotron -->
+    <div class="container">
+      <!-- Start Customer Container -->
+      <h2 class="text-center text-white">Happy Customers</h2>
+      <div class="row mt-5">
+        <div class="col-lg-3 col-sm-6">
+          <!-- Start Customer 1st Column-->
+          <div class="card shadow-lg mb-2">
+            <div class="card-body text-center">
+              <img src="images/avtar1.jpeg" class="img-fluid" style="border-radius: 100px;">
+              <h4 class="card-title">Rahul Kumar</h4>
+              <p class="card-text">Itaque illo explicabo voluptatum, saepe libero rerum, ad
+                ducimus voluptas
+                nesciunt debitis numquam.</p>
+            </div>
+          </div>
+        </div> <!-- End Customer 1st Column-->
+
+        <div class="col-lg-3 col-sm-6">
+          <!-- Start Customer 2nd Column-->
+          <div class="card shadow-lg mb-2">
+            <div class="card-body text-center">
+              <img src="images/avtar2.jpeg" class="img-fluid" style="border-radius: 100px;">
+              <h4 class="card-title">Sonam Sharma</h4>
+              <p class="card-text">Itaque illo explicabo voluptatum, saepe libero rerum, ad
+                ducimus voluptas
+                nesciunt debitis numquam.</p>
+            </div>
+          </div>
+        </div> <!-- End Customer 2nd Column-->
+
+        <div class="col-lg-3 col-sm-6">
+          <!-- Start Customer 3rd Column-->
+          <div class="card shadow-lg mb-2">
+            <div class="card-body text-center">
+              <img src="images/avtar3.jpeg" class="img-fluid" style="border-radius: 100px;">
+              <h4 class="card-title">Sumit Vyas</h4>
+              <p class="card-text">Itaque illo explicabo voluptatum, saepe libero rerum, ad
+                ducimus voluptas
+                nesciunt debitis numquam.</p>
+            </div>
+          </div>
+        </div> <!-- End Customer 3rd Column-->
+
+        <div class="col-lg-3 col-sm-6">
+          <!-- Start Customer 4th Column-->
+          <div class="card shadow-lg mb-2">
+            <div class="card-body text-center">
+              <img src="images/avtar4.jpeg" class="img-fluid" style="border-radius: 100px;">
+              <h4 class="card-title">Jyoti Sinha</h4>
+              <p class="card-text">Itaque illo explicabo voluptatum, saepe libero rerum, ad
+                ducimus voluptas
+                nesciunt debitis numquam.</p>
+            </div>
+          </div>
+        </div> <!-- End Customer 4th Column-->
+      </div> <!-- End Customer Row-->
+    </div> <!-- End Customer Container -->
+  </div> <!-- End Customer Jumbotron -->
+
+  <!--Start Contact Us-->
+  <div class="container" id="Contact">
+    <!--Start Contact Us Container-->
+    <h2 class="text-center mb-4">Contact US</h2> <!-- Contact Us Heading -->
+    <div class="row">
+
+      <!--Start Contact Us Row-->
+      <?php include('contactform.php'); ?>
+      <!-- End Contact Us 1st Column -->
+
+      <div class="col-md-4 text-center">
+        <!-- Start Contact Us 2nd Column-->
+        <strong>Office:</strong> <br>
+        Q-one Tech Solution, <br>
+        Parasmani Mall Jayendra Ganj <br>
+        Gwalior - 474009 <br>
+        Phone: +918269439060 <br>
+        <a href="#" target="_blank">www.qonetech.in</a> <br>
+
+        <br><br>
+        <strong>Branch:</strong> <br>
+        Q-one Technology, <br>
+        Shabd Paratp Ashram, Gwalior <br>
+        M.P - 474012 <br>
+        Phone: +918269439060 <br>
+        <a href="#" target="_blank">www.qonetech.in</a> <br>
+      </div> <!-- End Contact Us 2nd Column-->
+    </div> <!-- End Contact Us Row-->
+  </div> <!-- End Contact Us Container-->
+  <!-- End Contact Us -->
+
+  <!-- Start Footer-->
+  <footer class="container-fluid bg-dark text-white mt-5" style="border-top: 3px solid #DC3545;">
+    <div class="container">
+      <!-- Start Footer Container -->
+      <div class="row py-3">
+        <!-- Start Footer Row -->
+        <div class="col-md-6">
+          <!-- Start Footer 1st Column -->
+          <span class="pr-2">Follow Us: </span>
+          <a href="https://www.facebook.com/Q-one-Technology-109536294078050/?modal=admin_todo_tour" target="_blank" class="pr-2 fi-color"><i class="fab fa-facebook-f"></i></a>
+          <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-twitter"></i></a>
+          <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-youtube"></i></a>
+          <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-google-plus-g"></i></a>
+          <a href="#" target="_blank" class="pr-2 fi-color"><i class="fas fa-rss"></i></a>
+        </div> <!-- End Footer 1st Column -->
+
+        <div class="col-md-6 text-right">
+          <!-- Start Footer 2nd Column -->
+          <small> Designed by Kapil Kushwah &copy; 2020.
+          </small>
+          <small class="ml-2"><a href="Admin/login.php">Admin Login</a></small>
+        </div> <!-- End Footer 2nd Column -->
+      </div> <!-- End Footer Row -->
+    </div> <!-- End Footer Container -->
+  </footer> <!-- End Footer -->
+
+  <!-- Boostrap JavaScript -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/all.min.js"></script>
+</body>
+
+</html>
